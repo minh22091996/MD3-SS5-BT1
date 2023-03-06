142b22c13aafba470fcad9c7ece2ac6bcb8871f7
minh22091996 <tranbinhminh22091996@gmail.com>
2023-03-06 14:43:10 +0700

    Initial commit

---
+++ src/RIKKEI/TestCircle.java
@@ -0,0 +1,11 @@
+package RIKKEI;
+
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle circle = new Circle(4);
+        System.out.println(circle.getArea());
+        System.out.println(circle.getRadius());
+    }
+
+
+}
